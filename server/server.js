04b7172affc56a8b595e8660553563d22047c49a
Mikey Davison <mikeydavison@Mikeys-MacBook.local>
2018-07-05 12:02:05 -0700

    Add Post /users route and jwt token

--- server/server.js
+++ server/server.js
@@ -97,6 +97,18 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+app.post('/users', (req, res) => {
+  var user = new User(_.pick(req.body, ['email', 'password']));
+
+  user.save().then((user) => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
+  })
+});
+
 app.listen(port, () => {
     console.log(`Started on port: ${port}`);
 });
