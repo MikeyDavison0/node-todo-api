c53f834e60a0628ee116840b31b02ac70e229c31
Mikey Davison <mikeydavison@Mikeys-MacBook.local>
2018-07-05 15:14:46 -0700

    changes

---
+++ playground/hashing.js
@@ -0,0 +1,26 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = 'abc123';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$i8aJaiYKHxGw8z234.UvlufLnDtECZ0rPkhI8.85CeTSbhBWHShgC';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(`${token}`);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(`${decoded}`)
